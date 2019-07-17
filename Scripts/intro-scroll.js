8d2e0ba8f0e789a926325ac8a5aa056a325e13c3
ZombiMigz <chunyumikey@gmail.com>
2019-07-17 00:12:34 -0700

    sets responsive landing pg / outlines navbar

---
+++ Scripts/intro-scroll.js
@@ -0,0 +1,9 @@
+/*
+function restoreAndSkipContent() {
+    var hidden = document.getElementsByClassName('jump')[0];
+
+    hidden.classList.add('jump2');
+    window.scroll(0, hidden.offsetHeight);
+  };
+  restoreAndSkipContent();
+*/
